718b8a3cf57d454c03875ebd162fd6702e46c62d
Kento75 <kento2github@gmail.com>
2019-09-27 08:09:03 +0900

    Added 404 Error Route and Page

--- src/main/java/guru/springframework/spring5recipeapp/contorollers/RecipeController.java
+++ src/main/java/guru/springframework/spring5recipeapp/contorollers/RecipeController.java
@@ -1,11 +1,16 @@
 package guru.springframework.spring5recipeapp.contorollers;
 
 import guru.springframework.spring5recipeapp.commands.RecipeCommand;
+import guru.springframework.spring5recipeapp.exceptions.NotFoundException;
 import guru.springframework.spring5recipeapp.services.RecipeService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
+@Slf4j
 @Controller
 public class RecipeController {
 
@@ -50,5 +55,16 @@ public class RecipeController {
         recipeService.deleteById(Long.valueOf(id));
 
         return "redirect:/";
+    }
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(NotFoundException.class)
+    public ModelAndView handleNotFound() {
+        log.error("Handling not found exception");
+        ModelAndView modelAndView = new ModelAndView();
+
+        modelAndView.setViewName("404error");
+
+        return modelAndView;
     }
 }
